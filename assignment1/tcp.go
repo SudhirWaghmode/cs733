a8b18fd2c31e21a9ceaadc7474be0bb54cd9ccde
SudhirWaghmode <SudhirWaghmode@users.noreply.github.com>
2015-02-01 21:52:52 +0530

    Update tcp.go

--- assignment1/tcp.go
+++ assignment1/tcp.go
@@ -1,20 +1,21 @@
 package assignment1
+
 import (
 	"net"
 	"os"
 	"sync"
 	"strings"
 	"fmt"
+	"time"
 	"strconv"
 )
 
 type Node struct {
-	expt int64
+	expt string
 	numByte int64
 	value string
 	version int64
-//	timeStamp int64
-
+	timeStamp time.Time
 }
 
 var m map[string]Node
@@ -67,14 +68,33 @@ func handleClient(conn net.Conn) {
 				if err2 != nil {
 					return
 				}
-			
+		case "cas":
+			 mutex.Lock()	
+				 cas_body(s)
+			 mutex.Unlock()	
+				_, err2 := conn.Write([]byte("OK"+ strconv.FormatInt(m[s[1]].version,10) +"\r\n"))
+				if err2 != nil {
+					return
+				}	
 		case "get":
 			_,ok:=m[s[1]]
 			if ok{
-			_, err2 := conn.Write([]byte("VALUE "+strconv.FormatInt(m[s[1]].numByte,10)+" \r\n"+m[s[1]].value+"\r\n"))
-				if err2 != nil {
-					return
-				}
+					dur,_:=time.ParseDuration(m[s[1]].expt)
+				
+					if dur > time.Now().Sub(m[s[1]].timeStamp){						
+						_, err2 := conn.Write([]byte("VALUE "+strconv.FormatInt(m[s[1]].numByte,10)+" \r\n"+m[s[1]].value+"\r\n"))
+						if err2 != nil {
+							return
+						}//internal if
+					}else{
+							mutex.Lock()	
+								delete(m,string(s[1]))
+							mutex.Unlock()	
+							_,err2:= conn.Write([]byte("Error"))
+							if err2 != nil {
+								return
+							}//internal if		
+					}//timestamp if else
 			}else{
 				_,err2:= conn.Write([]byte("Error"))
 				if err2 != nil {
@@ -83,12 +103,27 @@ func handleClient(conn net.Conn) {
 			}
 		case "getm":
 			_,ok:=m[s[1]]
-			if ok{
-			_, err2 := conn.Write([]byte("VALUE "+strconv.FormatInt(m[s[1]].version,10)+" "+strconv.FormatInt(m[s[1]].expt,10)+" "+strconv.FormatInt(m[s[1]].numByte,10)+"\r\n"+m[s[1]].value+"\r\n"))
-			if err2 != nil {
-					return
-				}
-			}
+			if ok{				
+				dur,_:=time.ParseDuration(m[s[1]].expt)
+				
+					if dur > time.Now().Sub(m[s[1]].timeStamp){						
+						_, err2 := conn.Write([]byte("VALUE "+strconv.FormatInt(m[s[1]].version,10)+" "+m[s[1]].expt+" "+strconv.FormatInt(m[s[1]].numByte,10)+"\r\n"+m[s[1]].value+"\r\n"))
+						if err2 != nil {
+								return
+							}////error if
+					}else{
+							_,err2:= conn.Write([]byte("Error"))
+							if err2 != nil {
+								return
+							}////error  if
+					}
+
+			}else{
+					_,err2:= conn.Write([]byte("Error"))
+					if err2 != nil {
+						return
+					}////error  if
+			}///outer if
 		case "delete":
 				_,ok:=m[s[1]]
 				mutex.Lock()	
@@ -109,13 +144,9 @@ func handleClient(conn net.Conn) {
 	}
 }
 
-
-func set_body(s []string) {
-		k := strings.Split(string(s[3]),"\r\n")
-		int1,err :=(strconv.Atoi(s[2]))
-		if err != nil{
-			fmt.Println("sa")	
-		} 
+func cas_body(s []string) {
+		k := strings.Split(string(s[4]),"\r\n")
+		int1,_ :=(strconv.Atoi(s[3]))
 		int2,err :=(strconv.Atoi(k[0]))
 		if err != nil{
 		fmt.Println("sa")	
@@ -123,10 +154,32 @@ func set_body(s []string) {
 		_,ok:=m[s[1]]
 		
 		if !ok {
-			m[s[1]]=Node{int64(int1),int64(int2),k[1],int64(1)}
+			//fmt.Println("No Data")
+		}else{
+			if m[s[1]].version < int64(int1){
+				m[s[1]]=Node{s[2]+"s",int64(int2),k[1],int64(int1),time.Now()}
+			}else{
+				//fmt.Println("No Updat")
+			}
+		}
+		
+}
+
+func set_body(s []string) {
+		k := strings.Split(string(s[3]),"\r\n")
+		
+		int2,err :=(strconv.Atoi(k[0]))
+		if err != nil{
+		fmt.Println("sa")	
+		} 
+		_,ok:=m[s[1]]
+		
+		if !ok {
+
+			m[s[1]]=Node{s[2]+"s",int64(int2),k[1],int64(1),time.Now()}
 		}else{
 			val:=m[s[1]].version
-			m[s[1]]=Node{int64(int1),int64(int2),k[1],val+1}
+			m[s[1]]=Node{s[2]+"s",int64(int2),k[1],val+1,time.Now()}
 		}
 		
 }
@@ -134,6 +187,7 @@ func set_body(s []string) {
 func checkError(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-		os.Exit(1)
+	
 	}
 }
+
